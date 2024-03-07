a254d1ecc69fcd327b078e6fe80d1029e377b289
vishnupratap3790 <pratapvishnu2001@gmail.com>
2024-03-07 16:37:55 +0530

    second commit

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -2,6 +2,7 @@ import {io} from 'socket.io-client';
 import React from 'react';
 const SOCKET_URL = "https://chatapp-backend-x37e.onrender.com";
 
-export const socket = io(SOCKET_URL);
+export const socket = io(SOCKET_URL,  {transports: ['polling']});
+ 
 
 export const AppContext = React.createContext();
